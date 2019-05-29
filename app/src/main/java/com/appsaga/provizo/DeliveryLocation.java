acf891ac19009f2f3dc2a192f377ca91b525a3a9
aggarwalmayank <17dcs006@lnmiit.ac.in>
2019-05-29 20:04:12 +0530

    places api

--- app/src/main/java/com/appsaga/provizo/DeliveryLocation.java
+++ app/src/main/java/com/appsaga/provizo/DeliveryLocation.java
@@ -8,12 +8,17 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -25,6 +30,11 @@ public class DeliveryLocation extends AppCompatActivity {
     static final int DIALOG_ID = 0;
     ImageButton deliveryNext;
     EditText pickupdate;
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
 
 
     @Override
@@ -47,6 +57,9 @@ public class DeliveryLocation extends AppCompatActivity {
                 showDialog(DIALOG_ID);
             }
         });
+        if(isServicesOK()){
+            init();
+        }
 
         deliveryNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,4 +121,44 @@ public class DeliveryLocation extends AppCompatActivity {
         });
         builder.show();
     }
+    private void init(){
+        EditText pickup= (EditText) findViewById(R.id.pickuploc);
+        EditText drop= (EditText) findViewById(R.id.droploc);
+        pickup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DeliveryLocation.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        drop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DeliveryLocation.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(DeliveryLocation.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(DeliveryLocation.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
 }

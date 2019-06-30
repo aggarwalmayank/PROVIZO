effe6eaeec2c618101881fddb9fded6c9156c534
aggarwalmayank <17dcs006@lnmiit.ac.in>
2019-06-30 12:31:04 +0530

    anim

--- app/src/main/java/com/appsaga/provizo/SplashScreenCircular.java
+++ app/src/main/java/com/appsaga/provizo/SplashScreenCircular.java
@@ -3,6 +3,7 @@ package com.appsaga.provizo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -15,14 +16,16 @@ import android.widget.TextView;
 
 public class SplashScreenCircular extends AppCompatActivity {
 
-    //Button proText;
+    TextView proText;
     RelativeLayout splashRelative;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_circular);
-
+        proText = findViewById(R.id.pro_text);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/copperplatebold.ttf");
+        proText.setTypeface(typeface);
         splashRelative = findViewById(R.id.splash_relative);
 
         splashRelative.post(new Runnable() {
@@ -34,8 +37,7 @@ public class SplashScreenCircular extends AppCompatActivity {
         });
     }
 
-    public void performAnimation()
-    {
+    public void performAnimation() {
         //splashRelative = findViewById(R.id.pro_text);
 
         final int cx = splashRelative.getWidth() / 2;
@@ -43,10 +45,10 @@ public class SplashScreenCircular extends AppCompatActivity {
 
         float finalRadius = (float) Math.hypot(cx, cy);
 
-        Animator anim ;
+        Animator anim;
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-            anim = ViewAnimationUtils.createCircularReveal(splashRelative, cx, cy, 0, finalRadius);
+            anim = ViewAnimationUtils.createCircularReveal(splashRelative, cx, cy,  finalRadius,0);
 
             splashRelative.setVisibility(View.VISIBLE);
             anim.start();
@@ -62,7 +64,7 @@ public class SplashScreenCircular extends AppCompatActivity {
 
                             float initialRadius = (float) Math.hypot(cx, cy);
 
-                            Animator finalAnim = ViewAnimationUtils.createCircularReveal(splashRelative, cx, cy, initialRadius,0) ;
+                            Animator finalAnim = ViewAnimationUtils.createCircularReveal(splashRelative, cx, cy, 0, initialRadius);
                             finalAnim.start();
 
                             finalAnim.addListener(new AnimatorListenerAdapter() {
@@ -76,7 +78,7 @@ public class SplashScreenCircular extends AppCompatActivity {
                                 }
                             });
                         }
-                    },1500);
+                    }, 1500);
                 }
             });
         }

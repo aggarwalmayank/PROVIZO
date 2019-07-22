44014317ab41c242842aa2dd685c5cc2bf57edab
17ucs042 <32924261+17ucs042@users.noreply.github.com>
2019-07-22 18:55:14 +0530

    Truck booking app

--- app/src/main/java/com/appsaga/provizo/SplashScreenCircular.java
+++ app/src/main/java/com/appsaga/provizo/SplashScreenCircular.java
@@ -3,6 +3,7 @@ package com.appsaga.provizo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
@@ -14,10 +15,21 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.play.core.appupdate.AppUpdateInfo;
+import com.google.android.play.core.appupdate.AppUpdateManager;
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory;
+import com.google.android.play.core.install.model.AppUpdateType;
+import com.google.android.play.core.install.model.UpdateAvailability;
+import com.google.android.play.core.tasks.OnSuccessListener;
+import com.google.android.play.core.tasks.Task;
+
+import static com.sun.activation.registries.LogSupport.log;
+
 public class SplashScreenCircular extends AppCompatActivity {
 
     TextView proText;
     RelativeLayout splashRelative;
+    final int MY_REQUEST_CODE = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,7 @@ public class SplashScreenCircular extends AppCompatActivity {
             @Override
             public void run() {
 
+                checkForUpdates();
                 performAnimation();
             }
         });
@@ -84,5 +97,51 @@ public class SplashScreenCircular extends AppCompatActivity {
         }
     }
 
+    void checkForUpdates()
+    {
+        final AppUpdateManager appUpdateManager = AppUpdateManagerFactory.create(SplashScreenCircular.this);
+
+        // Returns an intent object that you use to check for an update.
+        Task<AppUpdateInfo> appUpdateInfoTask = appUpdateManager.getAppUpdateInfo();
+
+        // Checks that the platform will allow the specified type of update.
+
+        appUpdateInfoTask.addOnSuccessListener(new OnSuccessListener<AppUpdateInfo>() {
+            @Override
+            public void onSuccess(AppUpdateInfo appUpdateInfo) {
+
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                        // For a flexible update, use AppUpdateType.FLEXIBLE
+                        && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+
+                    try {
+                        appUpdateManager.startUpdateFlowForResult(
+                                // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                                appUpdateInfo,
+                                // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                                AppUpdateType.IMMEDIATE,
+                                // The current activity making the update request.
+                                SplashScreenCircular.this,
+                                // Include a request code to later monitor this update request.
+                                MY_REQUEST_CODE);
+                    } catch (IntentSender.SendIntentException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MY_REQUEST_CODE) {
+            if (resultCode != RESULT_OK) {
+                log("Update flow failed! Result code: " + resultCode);
+                // If the update is cancelled or fails,
+                // you can request to start the update again.
+                checkForUpdates();
+            }
+        }
+    }
 
 }
